fe254c85205880ffa955a833af7610247a114f1e
esalamanca <esalamanca@aduana.gob.bo>
2013-10-02 15:11:36 -0400

    Se agregaron anotaciones JPA a todas las Entidades del Modelo

---
+++ src/main/java/bo/gob/aduana/base/model/ManifiestoEstado.java
@@ -0,0 +1,63 @@
+package bo.gob.aduana.base.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "manifiesto_estado")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class ManifiestoEstado {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
+	private String estado;
+
+	@NotNull
+	private Date fecha;
+
+	@NotNull
+	private String usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+}
